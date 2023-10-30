16acab0744c4731012cd73de3833990ad27253b5
gsrcodes <guilhermesouza281@protonmail.com>
2023-10-30 09:26:09 -0300

    testando

---
+++ modelo/categoria.js
@@ -0,0 +1,44 @@
+class Categoria extends Object {
+    #codigo
+    #descricao
+
+    constructor (codigo = 0, descricao = '') {
+        this.#codigo = codigo;
+        this.#descricao = descricao;
+    }
+
+    get codigo() {
+        return this.#codigo;
+    }
+
+    set codigo(novoCodigo) {
+        this.#codigo = novoCodigo;
+    }
+
+    get descricao () {
+        return this.#descricao;
+    }
+
+    set descricao(novaDescricao) {
+        this.#descricao = novaDescricao;
+    }
+
+    toJSON() {
+        return {
+            codigo:this.#codigo,
+            descricao:this.#descricao
+        }
+    }
+
+    async gravar() {
+
+    }
+
+    async excluir() {
+
+    }
+
+    async consultar() {
+
+    }
+}
